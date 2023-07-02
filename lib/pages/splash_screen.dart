2cebfe59355f89367b5ddbda57d47663e265f81a
Wade <hhh@example.com>
2023-07-02 05:42:50 +0530

    Login Screen SignUp Screen

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:movieticketbookingapp/pages/login_screen.dart';
 import 'package:movieticketbookingapp/utils/mytheme.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -9,16 +13,45 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin{
+
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+
+  @override
+  void initState(){
+    _animationController = AnimationController(vsync: this, duration: const Duration(milliseconds: 1000));
+    _animation =
+        CurvedAnimation(parent: _animationController, curve: Curves.bounceInOut , reverseCurve: Curves.bounceInOut);
+    _animationController.forward();
+    super.initState();
+    
+    Timer(const Duration(milliseconds:2500 ), () => Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => LoginScreen())));
+
+  }
+
+  @override
+  void dispose(){
+    _animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(statusBarColor:Colors.transparent ));
     return Scaffold(
       backgroundColor: MyTheme.splash,
       body: Container(
-        child: Center(child: SvgPicture.asset('assets/icons/splash_icon.svg'),
+        child: Center(
+          child: ScaleTransition(
+          scale: _animation,
+          child: SvgPicture.asset(
+            'assets/icons/splash_icon.svg',
+          height:80
         ),
-
+        ),
       ),
+    )
     );
   }
 }
---
+++ lib/utils/social_button.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:movieticketbookingapp/utils/mytheme.dart';
+
+class SocialLoginButton extends StatelessWidget {
+  final Function() onGoogleClick;
+  final Function() onFbClick;
+  const SocialLoginButton ({Key? key, required this.onFbClick, required this.onGoogleClick}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Expanded(
+          child: Material(
+            color: MyTheme.redLight,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5),
+              side: BorderSide(color: MyTheme.redBorder, width: 0.2),
+            ),
+            child: InkWell(
+            onTap: onGoogleClick,
+                borderRadius: BorderRadius.circular(5),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+              child: Row(
+                children: [
+                  SvgPicture.asset("assets/icons/google.svg"),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  const Text("Google",
+                    style: TextStyle(
+                        color: Color(0xFF666666),
+                      fontWeight: FontWeight.w600,
+                      fontSize: 15
+                    ),
+                  ),
+                ],
+              ),
+            )
+        ),
+          ),
+        ),
+        const SizedBox(
+          width: 10,
+        ),
+        Expanded(
+          child: Material(
+            color: MyTheme.blueLight,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5),
+              side: BorderSide(color: MyTheme.blueBorder, width: 0.2),
+            ),
+            child: InkWell(
+                onTap: onFbClick,
+                borderRadius: BorderRadius.circular(5),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+                  child: Row(
+                    children: [
+                      SvgPicture.asset("assets/icons/facebook.svg"),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      const Text("Facebook",
+                        style: TextStyle(
+                          color: Color(0xFF666666),
+                          fontWeight: FontWeight.w600,
+                          fontSize: 15
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
